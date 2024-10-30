3a605b94b86d0d6eec4fcaba2df64bad2ad3bfae
Vyacheslav Egorov <vegorov@google.com>
2024-10-30 14:32:07 +0100

    Initial version

---
+++ bin/dart_lsp_collect_global_report.dart
@@ -0,0 +1,194 @@
+// Copyright (c) 2024. the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:vm_service/vm_service_io.dart' as vm_service;
+
+String prefixLines(String lines, {required String prefix}) {
+  return lines.split('\n').map((l) => '$prefix$l').join('\n');
+}
+
+Future<String> execute(String command,
+    [List<String> arguments = const <String>[]]) async {
+  final result = await Process.run(command, arguments);
+  if (result.exitCode != 0) {
+    stderr.writeln(
+        'running $command ${arguments.join(' ')} failed with ${result.exitCode}');
+    stderr.writeln(prefixLines(prefix: 'stdout', result.stdout as String));
+    stderr.writeln(prefixLines(prefix: 'stderr', result.stderr as String));
+    exit(1);
+  }
+  return result.stdout as String;
+}
+
+Future<Map<int, String>> getProcesses() async {
+  final output = await execute('ps', ['xo', 'pid=,command=']);
+  return Map.fromEntries(output
+      .split('\n')
+      .map((line) => line.trim())
+      .where((line) => line.isNotEmpty)
+      .map((line) {
+    final spacePos = line.indexOf(' ');
+    final pid = line.substring(0, spacePos);
+    final command = line.substring(spacePos + 1);
+    return MapEntry(int.parse(pid), command);
+  }));
+}
+
+Future<Map<int, String>> getDartProcesses(String kind) async {
+  final allProcesses = await getProcesses();
+  final filter = 'dart $kind';
+  return {
+    for (var e in allProcesses.entries)
+      if (e.value.contains(filter)) e.key: e.value,
+  };
+}
+
+Future<Map<int, String>> getDartLspProcesses() async {
+  return await getDartProcesses('language-server');
+}
+
+Future<Map<int, String>> getDartDDSProcesses() async {
+  return await getDartProcesses('development-service');
+}
+
+Future<Map<int, int>> getPortToPidMapping() async {
+  final output = await execute('lsof', ['-iTCP', '-sTCP:LISTEN', '-Fpn']);
+  late int currentPid;
+  final result = <int, int>{};
+  for (var line in output.split('\n')) {
+    if (line.startsWith('p')) {
+      currentPid = int.parse(line.substring(1));
+    } else if (line.startsWith('nlocalhost:')) {
+      if (int.tryParse(line.substring('nlocalhost:'.length)) case final port?) {
+        result[port] = currentPid;
+      }
+    }
+  }
+  return result;
+}
+
+Future<Map<int, Uri>> getVmServiceUris(Map<int, String> lspProcesses) async {
+  final result = <int, Uri>{};
+  print('Finding all open ports');
+  final portToPid = await getPortToPidMapping();
+  print('Checking for development-service processes');
+  final ddsProcesses = await getDartDDSProcesses();
+  for (var dds in ddsProcesses.entries) {
+    final vmServiceUriString = dds.value
+        .split(' ')
+        .firstWhereOrNull((arg) => arg.startsWith('--vm-service-uri='))
+        ?.substring('--vm-service-uri='.length);
+    final vmServiceUri =
+        vmServiceUriString != null ? Uri.tryParse(vmServiceUriString) : null;
+    if (vmServiceUri == null) {
+      print(
+          'Unable to determine VM Service URI from DDS process: ${dds.value}');
+      continue;
+    }
+    if (portToPid[vmServiceUri.port] case final targetPid?
+        when lspProcesses.containsKey(targetPid)) {
+      result[targetPid] = vmServiceUri.replace(scheme: 'ws').resolve('ws');
+    }
+  }
+  return result;
+}
+
+Future<Map<String, dynamic>> collectDataFrom(Uri serviceUri) async {
+  Map<String, dynamic> collectedData = {};
+  var serviceClient =
+      await vm_service.vmServiceConnectUri(serviceUri.toString());
+  try {
+    var vm = await serviceClient.getVM();
+    collectedData['vm.architectureBits'] = vm.architectureBits;
+    collectedData['vm.hostCPU'] = vm.hostCPU;
+    collectedData['vm.operatingSystem'] = vm.operatingSystem;
+    collectedData['vm.startTime'] = vm.startTime;
+
+    var processMemoryUsage = await serviceClient.getProcessMemoryUsage();
+    collectedData['processMemoryUsage'] = processMemoryUsage.json;
+
+    var isolateData = [];
+    collectedData['isolates'] = isolateData;
+    var isolates = [...?vm.isolates, ...?vm.systemIsolates];
+    for (var isolate in isolates) {
+      if (isolate.name == 'vm-service' ||
+          isolate.name == 'kernel-service' ||
+          isolate.name == 'dartdev') {
+        continue;
+      }
+      String? id = isolate.id;
+      if (id == null) continue;
+      var thisIsolateData = {};
+      isolateData.add(thisIsolateData);
+      thisIsolateData['id'] = id;
+      thisIsolateData['isolateGroupId'] = isolate.isolateGroupId;
+      thisIsolateData['name'] = isolate.name;
+      var isolateMemoryUsage = await serviceClient.getMemoryUsage(id);
+      thisIsolateData['memory'] = isolateMemoryUsage.json;
+      var allocationProfile = await serviceClient.getAllocationProfile(id);
+      var allocationMembers = allocationProfile.members ?? [];
+      var allocationProfileData = [];
+      thisIsolateData['allocationProfile'] = allocationProfileData;
+      for (var member in allocationMembers) {
+        var bytesCurrent = member.bytesCurrent;
+        // Filter out very small entries to avoid the report becoming too big.
+        if (bytesCurrent == null || bytesCurrent < 1024) continue;
+
+        var memberData = {};
+        allocationProfileData.add(memberData);
+        memberData['bytesCurrent'] = bytesCurrent;
+        memberData['instancesCurrent'] = member.instancesCurrent;
+        memberData['accumulatedSize'] = member.accumulatedSize;
+        memberData['instancesAccumulated'] = member.instancesAccumulated;
+        memberData['className'] = member.classRef?.name;
+        memberData['libraryName'] = member.classRef?.library?.name;
+      }
+      allocationProfileData.sort((a, b) {
+        int bytesCurrentA = a['bytesCurrent'] as int;
+        int bytesCurrentB = b['bytesCurrent'] as int;
+        // Largest first.
+        return bytesCurrentB.compareTo(bytesCurrentA);
+      });
+    }
+    return collectedData;
+  } finally {
+    await serviceClient.dispose();
+  }
+}
+
+void main(List<String> arguments) async {
+  print('Finding Dart LSP processes');
+  final lspProcesses = await getDartLspProcesses();
+  var uris = await getVmServiceUris(lspProcesses);
+  for (var lspWithoutDDS
+      in Set.of(lspProcesses.keys).difference(Set.of(uris.keys))) {
+    print('Sending SIGQUIT to $lspWithoutDDS in attempt to start vm-service');
+    try {
+      execute('kill', ['-QUIT', '$lspWithoutDDS']);
+      print('... OK');
+    } catch (_) {
+      print('... FAILED');
+    }
+  }
+  uris = await getVmServiceUris(lspProcesses);
+
+  final data = <String, dynamic>{};
+  for (var entry in uris.entries) {
+    print(
+        'Trying to fetch data from LSP process ${entry.key} via ${entry.value}');
+    try {
+      data[entry.key.toString()] = await collectDataFrom(entry.value);
+      print('... OK');
+    } catch (_) {
+      print('... FAILED');
+    }
+  }
+  File('lsp-report.json')
+      .writeAsStringSync(JsonEncoder.withIndent('  ').convert(data));
+  print('SUCCESS: written lsp-report.json');
+}
